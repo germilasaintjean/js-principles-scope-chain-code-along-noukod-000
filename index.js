a2d306edffc43643fe95ff4afc15fe1076ef9f50
Saint-Jean Germila <germila@noukod.com>
2019-03-12 15:03:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function overWriteBestCustomer(newFavorite){
   bestCustomer = newFavorite
 }
 
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
-}
-
-function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
-}
+// function congratulateCustomer(){
+//   return `congrats ${favoriteCustomer}`
+// }
+// 
+// function attemptTwoFavoriteCustomers(){
+//     let favoriteCustomer = 'bar'
+//     let favoriteCustomer = 'not bar'
+// }
